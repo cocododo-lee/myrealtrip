673ad485c6d839c87ee473b1ced59b72f2ca67e7
cocododoe_lee <cocododo_lee@naver.com>
2024-08-13 22:38:08 +0900

    feat : vanilla-extract 패키지 스타일링 적용 테스트 및 불필요한코드 라인 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import {style} from './styles.css.ts';
+import * as styles from './styles.css.ts'; 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={styles.container}>
+        <h1 className={styles.exampleH1}>Vite + React + Typescript + MSW + zustand + vanilla-extract</h1>
       </div>
-      <h1>Vite + React + Typescript + MSW + zustand + vanilla-extract</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
