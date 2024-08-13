829f7bff6857748b2e7a122281019a596d5aaa7e
cocododoe_lee <cocododo_lee@naver.com>
2024-08-13 22:44:17 +0900

    feat:React-Query import 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import {useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider} from '@tanstack/react-query';
 // import {style} from './styles.css.ts';
 import * as styles from './styles.css.ts'; 
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
@@ -7,6 +10,9 @@ function App() {
       <div className={styles.container}>
         <h1 className={styles.exampleH1}>Vite + React + Typescript + MSW + zustand + vanilla-extract</h1>
       </div>
+      <QueryClientProvider client={queryClient}>
+        {/*  */}
+      </QueryClientProvider>
     </>
   )
 }
