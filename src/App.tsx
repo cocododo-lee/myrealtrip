57bed6f46abed8a84804b5b55c51e5d86210f61a
cocododoe_lee <cocododo_lee@naver.com>
2024-08-18 01:22:41 +0900

    feat: ReactQueryDevtools 셋팅_ react_query 다운그레이포함(5>4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import {useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {RouterProvider } from "react-router-dom";
 import router from "./router/router"; 
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient();
 
 const App:React.FC = ()=> {
@@ -10,6 +10,7 @@ const App:React.FC = ()=> {
     <>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen />
      </QueryClientProvider>
     </> 
   )
