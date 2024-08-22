d46c0e5867d9886f289952337273a20e7ef02c29
cocododoe_lee <cocododo_lee@naver.com>
2024-08-23 00:19:35 +0900

    feat : 데일리 커밋(중간) : 버튼 컴포넌트 데이터 선언 설정

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-// import  Buttons from "./atoms/Buttons";
+import  Buttons from '../components/atoms/Buttons'
 const Home:React.FC = () => {
   return (
     <div>
         <h1>Home</h1>
-        {/* <Buttons btntext=""/> */}
+        <Buttons
+      width="150px"
+      height="30px"
+      bgColor="green"
+      fontColor="indigo"
+      fontSize="16px"
+      >Button Clone Coding</Buttons>
     </div>
   )
 }
--- src/components/atoms/Buttons.tsx
+++ src/components/atoms/Buttons.tsx
@@ -1,40 +1,38 @@
-import React from 'react'
+import React, { ReactElement, ReactNode } from 'react'
+import {PaletteTypes} from '../../styles/palette'
 
-interface ButtonProps {
-  variants: [
-    {
-      //default 
-      default : string,
-      //Style
-      outline :string,
-      ghost : string,
-      icon : string,
-      round:string,
-    },
-  ],
-  role : {
-    default:string,
-    outlink : string,
-    form :string,
-    submit :string,
-  },
-  size : string,
-  background: string,
-  children : string,
-  onClick: () => void;
+interface ButtonStyled {
+  width?:string; // px을 받기 위한 
+  height?:string;
+  fontSize?: string; 
+  fontColor?:PaletteTypes;
+  hasBorder?: boolean;
+  borderColor?: PaletteTypes;
+  borderRadius?: string;
+  hasIcon?: true;
+  hasIconOnly?:true; // 아이콘만 보이는 버튼인 경우 
+  iconType?: string; // 클래스로 아이콘 클래스명 매칭을 위한 stirng 
+  bgColor?:PaletteTypes; // HEX코드 등을 받기 위한
+  // onClick?: () => void;
 }
 
+// 확장-상속
+interface ButtonProps extends ButtonStyled{
+  children: ReactNode;
+  className?: string;
+}
 
-const Buttons = (props:ButtonProps) => {
+const Buttons = ({className, children, ...rest}:ButtonProps):ReactElement => {
   return (
-    <>  
-      <button {...props}>
-        {props.children}
-      </button>
+    <>
+    <StyledButton className={className} {...rest}>{children}</StyledButton>
+      
+      {/* <ButtonStyled className={className} {...rest}>{children}</ButtonStyled> */}
 
-      {/* 0821 참고글 :
-          https://hackids.tistory.com/133
-       */}
+      {/* 공부용 코멘트는 최종 머지될 때 정리하여 삭제할 예정입니다! 
+          0821 참고글 : https://hackids.tistory.com/133
+          => ✅ 0821 멘토님 피드백 URL : https://doiler.tistory.com/m/40
+        */}
     </>
   )
 }
