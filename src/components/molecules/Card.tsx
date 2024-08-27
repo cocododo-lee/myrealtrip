cda76ef938302069c75e585d25ee39b5b6b20030
cocododoe_lee <cocododo_lee@naver.com>
2024-08-27 23:29:14 +0900

    feat: 아토믹디자인 기준 UI Element 분류 연습 (메인)

---
+++ src/components/molecules/Card.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>Card
+        <div className='card'>
+            <strong>파리</strong>
+            <button>둘러보기</button>
+        </div>
+    </div>
+  )
+}
+
+export default Card
